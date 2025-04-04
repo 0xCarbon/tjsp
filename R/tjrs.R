62284779905c601f698ba7090d829214ea490a95
Kenny Fukuya <kennyfukuya@gmail.com>
2025-04-04 10:07:43 -0300

    chore: removing class

--- R/tjrs.R
+++ R/tjrs.R
@@ -1,8 +1,7 @@
 #' Buscar jurisprudência no Tribunal de Justiça do RS
 #'
-#' Esta função permite buscar jurisprudência no Portal de Jurisprudência do Tribunal de Justiça do Estado do Rio Grande do Sul (TJRS) com base na classe processual e no período de julgamento.
+#' Esta função permite buscar jurisprudência no Portal de Jurisprudência do Tribunal de Justiça do Estado do Rio Grande do Sul (TJRS) com base no período de julgamento.
 #'
-#' @param classe Classe processual para a qual deseja-se buscar jurisprudência.
 #' @param julgamento_inicial Data de julgamento inicial das decisões a serem buscadas (no formato "DD/MM/YYYY"). O padrão é "" (vazio), indicando que não há restrição de data inicial.
 #' @param julgamento_final Data de julgamento final das decisões a serem buscadas (no formato "DD/MM/YYYY"). O padrão é "" (vazio), indicando que não há restrição de data final.
 #' @param .reportar Se TRUE, exibe uma mensagem informando sobre os parâmetros da busca realizada. O padrão é TRUE.
@@ -14,25 +13,24 @@
 #'
 #' @examples
 #' \dontrun{
-#' # Buscar jurisprudência com base na classe processual
-#' tjrs <- tjrs_jurisprudencia(classe = "Direta de Inconstitucionalidade", julgamento_inicial = "01/01/2023", julgamento_final = "31/03/2023")
+#' # Buscar jurisprudência tjrs
+#' tjrs <- tjrs_jurisprudencia(julgamento_inicial = "01/01/2023", julgamento_final = "31/03/2023")
 #' tjrs |> head(5)
 #'
 #' # Caso ele não encontre nada, mostrará e um aviso e retornará um valor NULL
-#' tjrs_jurisprudencia(classe = "Direta de Inconstitucionalidade", julgamento_inicial = "01/01/2023", julgamento_final = "31/02/2023")
+#' tjrs_jurisprudencia(julgamento_inicial = "01/01/2023", julgamento_final = "31/02/2023")
 #' }
-tjrs_jurisprudencia <- function(classe, julgamento_inicial = "", julgamento_final = "", .reportar = TRUE) {
+tjrs_jurisprudencia <- function(julgamento_inicial = "", julgamento_final = "", .reportar = TRUE) {
   url <- "https://www.tjrs.jus.br/buscas/jurisprudencia/ajax.php"
 
   pagina <- 1
-  acao <- curl::curl_escape(classe)
   dt_julgamento_de <- curl::curl_escape(julgamento_inicial)
   dt_julgamento_ate <- curl::curl_escape(julgamento_final)
 
   parametros <- list(
     "action" = "consultas_solr_ajax",
     "metodo" = "buscar_resultados",
-    "parametros" = glue::glue("aba=jurisprudencia&realizando_pesquisa=1&pagina_atual=1&q_palavra_chave=&conteudo_busca=ementa_completa&filtroComAExpressao=&filtroComQualquerPalavra=&filtroSemAsPalavras=&filtroTribunal=-1&filtroRelator=-1&filtroOrgaoJulgador=-1&filtroTipoProcesso={acao}&filtroClasseCnj=-1&assuntoCnj=-1&data_julgamento_de={dt_julgamento_de}&data_julgamento_ate={dt_julgamento_ate}&filtroNumeroProcesso=&data_publicacao_de=&data_publicacao_ate=&facet=on&facet.sort=index&facet.limit=index&wt=json&ordem=desc&start=0")
+    "parametros" = glue::glue("aba=jurisprudencia&realizando_pesquisa=1&pagina_atual=1&q_palavra_chave=&conteudo_busca=ementa_completa&filtroComAExpressao=&filtroComQualquerPalavra=&filtroSemAsPalavras=&filtroTribunal=-1&filtroRelator=-1&filtroOrgaoJulgador=-1&filtroTipoProcesso=-1&filtroClasseCnj=-1&assuntoCnj=-1&data_julgamento_de={dt_julgamento_de}&data_julgamento_ate={dt_julgamento_ate}&filtroNumeroProcesso=&data_publicacao_de=&data_publicacao_ate=&facet=on&facet.sort=index&facet.limit=index&wt=json&ordem=desc&start=0")
   )
 
   res <- httr::POST(
@@ -63,7 +61,7 @@ tjrs_jurisprudencia <- function(classe, julgamento_inicial = "", julgamento_fina
     parametros <- list(
       "action" = "consultas_solr_ajax",
       "metodo" = "buscar_resultados",
-      "parametros" = glue::glue("aba=jurisprudencia&realizando_pesquisa=1&pagina_atual={pagina}&q_palavra_chave=&conteudo_busca=ementa_completa&filtroComAExpressao=&filtroComQualquerPalavra=&filtroSemAsPalavras=&filtroTribunal=-1&filtroRelator=-1&filtroOrgaoJulgador=-1&filtroTipoProcesso={acao}&filtroClasseCnj=-1&assuntoCnj=-1&data_julgamento_de={dt_julgamento_de}&data_julgamento_ate={dt_julgamento_ate}&filtroNumeroProcesso=&data_publicacao_de=&data_publicacao_ate=&facet=on&facet.sort=index&facet.limit=index&wt=json&ordem=desc&start=0")
+      "parametros" = glue::glue("aba=jurisprudencia&realizando_pesquisa=1&pagina_atual={pagina}&q_palavra_chave=&conteudo_busca=ementa_completa&filtroComAExpressao=&filtroComQualquerPalavra=&filtroSemAsPalavras=&filtroTribunal=-1&filtroRelator=-1&filtroOrgaoJulgador=-1&filtroTipoProcesso=-1&filtroClasseCnj=-1&assuntoCnj=-1&data_julgamento_de={dt_julgamento_de}&data_julgamento_ate={dt_julgamento_ate}&filtroNumeroProcesso=&data_publicacao_de=&data_publicacao_ate=&facet=on&facet.sort=index&facet.limit=index&wt=json&ordem=desc&start=0")
     )
 
     res <- httr::POST(
@@ -81,16 +79,16 @@ tjrs_jurisprudencia <- function(classe, julgamento_inicial = "", julgamento_fina
   if (.reportar == TRUE){
   if (julgamento_inicial == "") {
     glue::glue(
-      "Extrai do Portal de Jurisprudencia do Tribunal de Justica do Estado do Rio Grande do Sul todas as decisoes em {classe} proferidas ate {julgamento_final}.") |> print()
+      "Extrai do Portal de Jurisprudencia do Tribunal de Justica do Estado do Rio Grande do Sul todas as decisoes proferidas ate {julgamento_final}.") |> print()
   } else if (julgamento_final == ""){
     glue::glue(
-      "Extrai do Portal de Jurisprudencia do Tribunal de Justica do Estado do Rio Grande do Sul todas as decisoes em {classe} proferidas desde {julgamento_inicial}.") |> print()
+      "Extrai do Portal de Jurisprudencia do Tribunal de Justica do Estado do Rio Grande do Sul todas as decisoes proferidas desde {julgamento_inicial}.") |> print()
   } else if (julgamento_final == "" & julgamento_inicial == "") {
     glue::glue(
-      "Extrai do Portal de Jurisprudencia do Tribunal de Justica do Estado do Rio Grande do Sul todas as decisoes em {classe} proferidas.") |> print()
+      "Extrai do Portal de Jurisprudencia do Tribunal de Justica do Estado do Rio Grande do Sul todas as decisoes proferidas.") |> print()
   } else {
     glue::glue(
-      "Extrai do Portal de Jurisprudencia do Tribunal de Justica do Estado do Rio Grande do Sul todas as decisoes em {classe} proferidas entre {julgamento_inicial} e {julgamento_final}.") |> print()}}
+      "Extrai do Portal de Jurisprudencia do Tribunal de Justica do Estado do Rio Grande do Sul todas as decisoes proferidas entre {julgamento_inicial} e {julgamento_final}.") |> print()}}
 
   return(df)
 }
